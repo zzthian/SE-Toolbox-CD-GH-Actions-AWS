01df703fdb605b84e712f913b31310fd070d82f2
zzthian <zzthian@hotmail.com>
2023-10-13 17:52:36 +0800

    Add workflow and small changes

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api/addresses', require('./routes/addressRoutes')) // add
 
 // optional
 app.get('/', (req, res) => {
-  res.status(200).json("Welcome to Address Book with CD!")
+  res.status(200).json("Welcome to Address Book with CD! Please dont fail me")
 })
 
 app.listen(port, () => {
